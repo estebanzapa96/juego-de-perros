5822f289d09c4f83f6369d014c1b6f099d0e2ed8
estebanzapa96 <112511386+estebanzapa96@users.noreply.github.com>
2022-09-27 10:45:07 -0500

    Add files via upload
    ESTAMOS MELOS SIZAS

---
+++ juego/personajes.java
@@ -0,0 +1,54 @@
+import java.util.Random;
+
+public class personajes {
+
+    private int salud, ataque, mordida;
+    private String nombre;
+
+    private Random rand = new Random(System.nanoTime());
+
+    public int getSalud() {
+        return salud;
+    }
+    public void setSalud(int salud) {
+        this.salud = salud;
+    }
+    public int getFisico() {
+        return ataque;
+    }
+    public void setFisico(int fisico) {
+        this.ataque = fisico;
+    }
+    public int getMordida() {
+        return mordida;
+    }
+    public void setMordida(int mordida) {
+        this.mordida = mordida;
+    }
+    
+    
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    // PELEA
+    public int ataque_fisico(){
+        int aleatorio = rand.nextInt(4);
+        return mordida * ataque * aleatorio;
+    }
+
+    public int ataque_mordida(){
+        if(salud > 0){
+            return ataque * mordida;
+        }else{
+            return 0;
+        }
+    }
+
+    public void daño(int daño){
+        this.salud =this.salud-daño;
+    }
+}
